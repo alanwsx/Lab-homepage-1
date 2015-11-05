5ba6afc5dbd106864a6f400da219e84cde6efd7f
Jun <tobefuturer@gmail.com>
2015-11-05 19:46:57 +0800

    再换一次美美的照片

--- lab.py
+++ lab.py
@@ -2,12 +2,21 @@
 # -*- coding: utf-8 -*-
 
 import app
+import getopt, sys
+
 from flask import Flask
 
 import sys
 reload(sys)
 sys.setdefaultencoding('utf8')
 
+
 if __name__ == '__main__':
-    app.app.run(host='0.0.0.0',debug=True)
+    port=5000
+    opts, args = getopt.getopt(sys.argv[1:], '',["port="])
+    for opt, arg in opts:
+        if opt in ("--port"):
+            port=int(arg)
+
+    app.app.run(host='0.0.0.0',port=port,debug=True)
 
